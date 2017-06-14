e2c4daf0af085f6306229fcec05e42fe2710931a
Iván Ros <iros@datiobd.com>
2017-06-14 12:54:04 +0200

    Adding some form validations for Contact page

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
-export class ContactComponent implements OnInit {
+export class ContactComponent {
+
+  private contact = { };
+  private isSubmitted = false;
 
   constructor() { }
 
-  ngOnInit() {
+  onSubmit() {
+    this.isSubmitted = true;
   }
 
 }
